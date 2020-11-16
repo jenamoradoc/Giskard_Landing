b8d9e5eb83a1ac38cce345561915b4fd775317b5
David Enamorado <davidenamorado09@gmail.com>
2020-11-16 16:27:00 -0300

    changes video & footer

--- src/componentes/Main.js
+++ src/componentes/Main.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-import profile1 from "../img/Profile_1.png";
+import profile1 from "../img/Profile_1.jpg";
 import profile2 from "../img/Profile_2.png";
 import videoBackground from "../img/backgroundvideo2.mp4";
 import videoWebm from "../img/backgroundvideo2.webm";
-import photoframe1 from '../img/temporal_1.jpg'
-import photoframe2 from '../img/temporal_2.jpg'
-import photoframe3 from '../img/temporal_3.jpg'
+import photoframe1 from "../img/temporal_1.jpg";
+import photoframe2 from "../img/temporal_2.jpg";
+import photoframe3 from "../img/temporal_3.jpg";
+import Videos from "./Videos";
 
 class Main extends React.Component {
   render() {
@@ -16,8 +17,11 @@ class Main extends React.Component {
           <div class="u-center-text u-margin-bottom-big">
             <h2 class="heading-secondary">One Core to rule them all</h2>
             <p class="paragraph">
-			        Gracias a la disponibilidad de modulos independientes prediseñados, <br/>
-              construir un satelite sera como decidir de que sabor quieres tu pizza" </p>
+              Gracias a la disponibilidad de modulos independientes
+              prediseñados, <br />
+              construir un satelite sera como decidir de que sabor quieres tu
+              pizza"{" "}
+            </p>
           </div>
           <div class="row">
             <div class="col-2-of-3">
@@ -25,29 +29,40 @@ class Main extends React.Component {
                 Menos Costos
               </h3>
               <p class="paragraph">
-                Gran parte del costo de construccion de un satelite corresponde a los largos tiempos de desarrollo y el constante sorpote de un equipo de especialistas, lo que involucra grandes costos y suele desalentar estas iniciativas.
-                Gracias a los modulos preconstruidos de giskard podras despreocuparte por el proceso y el arduo proceso de investigacion.
+                Gran parte del costo de construccion de un satelite corresponde
+                a los largos tiempos de desarrollo y el constante sorpote de un
+                equipo de especialistas, lo que involucra grandes costos y suele
+                desalentar estas iniciativas. Gracias a los modulos
+                preconstruidos de giskard podras despreocuparte por el proceso y
+                el arduo proceso de investigacion.
               </p>
               <h3 class="heading-tertiary u-margin-bottom-small">
                 Mejores tiempos
               </h3>
               <p class="paragraph">
-                Una vez construido un satelite es necesario superar una etapa ardua y larga de pruebas y certificaciones antes de ser puesto en orbita, si, mas dinero.
-                Giskard orfrece un acompañamiento completo durante el proceso de diseño, simulacion de escenarios, manufcatura y certificacion de tus satelite basado en los modulos prediseñados.
+                Una vez construido un satelite es necesario superar una etapa
+                ardua y larga de pruebas y certificaciones antes de ser puesto
+                en orbita, si, mas dinero. Giskard orfrece un acompañamiento
+                completo durante el proceso de diseño, simulacion de escenarios,
+                manufcatura y certificacion de tus satelite basado en los
+                modulos prediseñados.
               </p>
-              <h3 class="heading-tertiary u-margin-bottom-small">
-                Mas Facil
-              </h3>
+              <h3 class="heading-tertiary u-margin-bottom-small">Mas Facil</h3>
               <p class="paragraph">
-                Mucha de la tecnologia satelital suele requerir una serie de funciones basicas, como comunicacion UHF, Computadora de vuelo, Gestion de Bateria, Camaras, Sensores, etc, 
-                Por ello estamos trabajando en brindarte cada vez mejores modulos que se adapten a tus proyectos espaciales.
+                Mucha de la tecnologia satelital suele requerir una serie de
+                funciones basicas, como comunicacion UHF, Computadora de vuelo,
+                Gestion de Bateria, Camaras, Sensores, etc, Por ello estamos
+                trabajando en brindarte cada vez mejores modulos que se adapten
+                a tus proyectos espaciales.
               </p>
-              <br/>
+              <br />
               <h3 class="heading-tertiary u-margin-bottom-small">
                 Ya estas listo para llegar al infinito y mas alla.
               </h3>
             </div>
-            <div class="col-1-of-3"> </div>
+            <div class="col-1-of-3"> 
+            <Videos/>
+            </div>
           </div>
         </section>
         <section class="section-about">
@@ -55,7 +70,7 @@ class Main extends React.Component {
             <div class="col-1-of-2">
               <div class="composition">
                 <img
-                 srcset={photoframe1}
+                  srcset={photoframe1}
                   sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
                   alt="Photo 1"
                   class="composition__photo composition__photo--p1"
@@ -63,7 +78,7 @@ class Main extends React.Component {
                 />
 
                 <img
-				 srcset={photoframe2}
+                  srcset={photoframe2}
                   sizes="(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px"
                   alt="Photo 2"
                   class="composition__photo composition__photo--p2"
@@ -85,10 +100,12 @@ class Main extends React.Component {
                 Te acompañamos durante todo el proceso
               </h3>
               <p class="paragraph">
-                Al adquirir un modulo Giskard estaras asesorado por un panel de expertos, que te permitiran ajustar el diseño a tus necesidades, asesorarte en el proceso de ensamblaje y certificacion, nuestro objetivo es que puedas enfocarte solo en lo mas importante, los resultados.
+                Al adquirir un modulo Giskard estaras asesorado por un panel de
+                expertos, que te permitiran ajustar el diseño a tus necesidades,
+                asesorarte en el proceso de ensamblaje y certificacion, nuestro
+                objetivo es que puedas enfocarte solo en lo mas importante, los
+                resultados.
               </p>
-
- 
             </div>
           </div>
         </section>
@@ -100,7 +117,8 @@ class Main extends React.Component {
                 <i class="feature-box__icon icon-basic-world"></i>
                 <h3 class="heading-tertiary u-margin-bottom-small">Diseño</h3>
                 <p class="feature-box__text">
-                  Los modulos de giskard son personalizables y nuestro equipo te ayudara a elegir el mas adecuado.
+                  Los modulos de giskard son personalizables y nuestro equipo te
+                  ayudara a elegir el mas adecuado.
                 </p>
               </div>
             </div>
@@ -112,7 +130,8 @@ class Main extends React.Component {
                   Simulacion
                 </h3>
                 <p class="feature-box__text">
-                  Es importante prepararnos para cualquier eventualidad, por lo que simulamos los escenarios y poder prepararnos.
+                  Es importante prepararnos para cualquier eventualidad, por lo
+                  que simulamos los escenarios y poder prepararnos.
                 </p>
               </div>
             </div>
@@ -124,7 +143,8 @@ class Main extends React.Component {
                   Manufacture
                 </h3>
                 <p class="feature-box__text">
-                  Gracias a la naturaleza modular y reutilizable, la fabricacion puede realizarse en periodos muy cortos.
+                  Gracias a la naturaleza modular y reutilizable, la fabricacion
+                  puede realizarse en periodos muy cortos.
                 </p>
               </div>
             </div>
@@ -132,9 +152,12 @@ class Main extends React.Component {
             <div class="col-1-of-4">
               <div class="feature-box">
                 <i class="feature-box__icon icon-basic-map"></i>
-                <h3 class="heading-tertiary u-margin-bottom-small">Test & Certificacion</h3>
+                <h3 class="heading-tertiary u-margin-bottom-small">
+                  Test & Certificacion
+                </h3>
                 <p class="feature-box__text">
-                  Te acompañamos en el proceso de certificacion y pruebas para que no tengas que preocuparte por eso.
+                  Te acompañamos en el proceso de certificacion y pruebas para
+                  que no tengas que preocuparte por eso.
                 </p>
               </div>
             </div>
@@ -156,7 +179,7 @@ class Main extends React.Component {
           </div>
           <div class="u-center-text u-margin-bottom-big">
             <h2 class="heading-secondary u-margin-top-big ">
-              What Experts Say?
+              Que dicen los Expertos?
             </h2>
           </div>
 
@@ -164,18 +187,17 @@ class Main extends React.Component {
             <div class="story">
               <figure class="story__shape">
                 <img src={profile1} alt="Person on a tour" class="story__img" />
-                <figcaption class="story__caption">James Marshall</figcaption>
+                <figcaption class="story__caption">Jakob Van Zyl</figcaption>
               </figure>
               <div class="story__text">
                 <h3 class="heading-tertiary u-margin-bottom-small">
-                  A effetive solution to a expensive problem
+                  A effetive solution
                 </h3>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Aperiam, ipsum sapiente aspernatur libero repellat quis
-                  consequatur ducimus quam nisi exercitationem omnis earum qui.
-                  Aperiam, ipsum sapiente aspernatur libero repellat quis
-                  consequatur ducimus quam nisi exercitationem omnis earum qui.
+                  "Cubesat technology creates a vehicle for people to do science
+                  at a much lower investment to the taxpayer." Jakob Van Zyl,
+                  director of the Solar System Exploration Directorate at NASA's
+                  Jet Propulsion Laboratory.
                 </p>
               </div>
             </div>
@@ -185,23 +207,21 @@ class Main extends React.Component {
             <div class="story">
               <figure class="story__shape">
                 <img src={profile2} alt="Person on a tour" class="story__img" />
-                <figcaption class="story__caption">Jack Wilson</figcaption>
+                <figcaption class="story__caption">John Baker</figcaption>
               </figure>
               <div class="story__text">
                 <h3 class="heading-tertiary u-margin-bottom-small">
-                  A sustainable and effective solution
+                  "There's big potential in these small packages,"
                 </h3>
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Aperiam, ipsum sapiente aspernatur libero repellat quis
-                  consequatur ducimus quam nisi exercitationem omnis earum qui.
-                  Aperiam, ipsum sapiente aspernatur libero repellat quis
-                  consequatur ducimus quam nisi exercitationem omnis earum qui.
+                  "CubeSats — part of a larger group of spacecraft called
+                  SmallSats — are a new platform for space exploration
+                  affordable to more than just government agencies." John Baker,
+                  the MarCO program manager at Jet Propulsion Lab.
                 </p>
               </div>
             </div>
           </div>
- 
         </section>
       </div>
     );
